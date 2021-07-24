ffa23a02ee96fae4291c7c38a7bd7a67adfc2d7f
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-07-23 20:42:13 -0700

    skills icons added

--- src/components/skill/skill.component.jsx
+++ src/components/skill/skill.component.jsx
@@ -8,23 +8,23 @@ const Skill = ({ identifier, technology, information, image }) => (
             identifier % 2 !== 0
                 ?
                 <div className="grid">
+                    <div>
+                        <img className="skill-image left-image" src={image} alt="" />
+                    </div>
                     <div className="skill-title-container left-side">
                         <div className="skill-title">{technology}</div>
                     </div>
                     <p className="skill-information">{information}</p>
-                    <div>
-                        <img className="skill-image" src={image} alt="" />
-                    </div>
                 </div>
                 :
                 <div className="grid">
+                    <div>
+                        <img className="skill-image right-image" src={image} alt="" />
+                    </div>
                     <div className="skill-title-container right-side">
                         <div className="skill-title">{technology}</div>
                     </div>
                     <p className="skill-information right-side">{information}</p>
-                    <div>
-                        <img className="skill-image" src={image} alt="" />
-                    </div>
                 </div>
         }
 
--- src/section/skills/skills.data.js
+++ src/section/skills/skills.data.js
@@ -10,36 +10,42 @@ const SKILLS_DATA = [
         id: 2,
         identifier: 2,
         technology: 'HTML/CSS/SCSS',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png',
         information: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. A provident illo fugiat natus.'
     },
     {
         id: 3,
         identifier: 3,
         technology: 'React.js',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png',
         information: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. A provident illo fugiat natus.'
     },
     {
         id: 4,
         identifier: 4,
         technology: 'MySQL',
+        image: 'https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-images-are-download-crazypng-21.png',
         information: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. A provident illo fugiat natus.'
     },
     {
         id: 5,
         identifier: 5,
         technology: 'MongoDB',
+        image: 'https://img.icons8.com/color/452/mongodb.png',
         information: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. A provident illo fugiat natus.'
     },
     {
         id: 6,
         identifier: 6,
         technology: 'Express.js',
+        image: 'https://pngimage.net/wp-content/uploads/2018/05/express-js-png-5.png',
         information: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. A provident illo fugiat natus.'
     },
     {
         id: 7,
         identifier: 7,
         technology: 'Node.js',
+        image: 'https://img.icons8.com/color/452/nodejs.png',
         information: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. A provident illo fugiat natus.'
     }
 ]
