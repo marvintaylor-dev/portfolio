5d8df6cd6e8932814e45f0bcf1372cc8f38aec3c
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-08-07 00:36:45 -0700

    removed .git files for front/back end scaffolding

---
+++ frontend/src/components/education-container/education-college/education-college.components.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './education-college.styles.scss'
+
+const College = () => (
+    <div className="college-class-container">
+        <section>
+            <img src='https://www.logolynx.com/images/logolynx/6c/6c18b4f7813f8bbacb28550f534a26c2.jpeg' alt="College-Logo" className="college-logo" />
+            <div className="degree-year-container">
+                <div className="degree-year-sectionOne">
+                    <div className="degree-heading">Degree</div>
+                    <div className="degree-name">Masters of Social Work</div>
+                </div>
+                <div className="degree-year-sectionTwo">
+                    <div className="year-heading">Year</div>
+                    <div className="year">2013</div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <img src='https://cdn.freelogovectors.net/wp-content/uploads/2020/10/middle-tennessee-state-university-logo-mtsu.png' alt="College-Logo" className="college-logo" />
+            <div className="degree-year-container">
+                <div className="degree-year-sectionOne">
+                    <div className="degree-heading">Degree</div>
+                    <div className="degree-name">Bachelors of Social Work</div>
+                </div>
+                <div className="degree-year-sectionTwo">
+                    <div className="year-heading">Year</div>
+                    <div className="year">2011</div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+)
+
+export default College;
---
+++ frontend/src/section/education/education.components.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import './education.styles.scss'
+
+import EDUCATION_DATA from '../../section/education/education.data'
+
+import EducationContainer from '../../components/education-container/education-container.components'
+
+class Education extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            educationData: EDUCATION_DATA
+        }
+    }
+
+    render() {
+        const { educationData } = this.state;
+        return (
+            <div className="education-page" id="education">
+                <div className="blue-box blue-box-left">
+                    <div className="circle-left"></div>
+                </div>
+                <div className="blue-box blue-box-right">
+                    <div className="circle-right"></div>
+                </div>
+                <div className="education-section-title">EDUCATION</div>
+                <div className="udemy-logo-container">
+                    <img src="https://www.udemy.com/staticx/udemy/images/v7/logo-udemy.svg" alt="udemy-logo" className="udemy-logo" />
+                </div>
+                {educationData.map(({ id, ...otherProps }) => (
+                    <EducationContainer key={id} {...otherProps} />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Education;
