4be4a3fc2123efddcbdc804ce1fce715464faaae
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-08-10 15:22:38 -0700

    intro change, homepage change

--- frontend/src/section/introduction/introduction.components.jsx
+++ frontend/src/section/introduction/introduction.components.jsx
@@ -17,7 +17,7 @@ export const Introduction = () => {
                     <div className="introduction-message__job-title">‘Full-Stack Web Developer’</div>
                 </div>
                 <p className="introduction-message__about-me">
-                    Hello! Welcome to my portfolio.  I’m a full stack developer using
+                    Hello! Welcome to my portfolio.  I’m a front-end web developer using
                     Javascript, React, Node, Express with experience in both MySQL
                     and MongoDB. My passion is In building projects that make
                     a difference.
