b554b8b090d964150c02c920f385234e9182585e
mt-dev <76923383+marvintaylor-dev@users.noreply.github.com>
2023-03-20 21:14:08 -0700

    Update contact.components.jsx

--- frontend/src/components/contact/contact.components.jsx
+++ frontend/src/components/contact/contact.components.jsx
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Contact() {
     return (
         <div className="icon-container">
-            <a href="https://www.linkedin.com/in/marvin-taylor-dev/" target="_blank"><FontAwesomeIcon id="awesomeIcon" size="3x" icon={["fab", "linkedin"]} /></a>
+            <a href="https://www.linkedin.com/in/marvtaylor/" target="_blank"><FontAwesomeIcon id="awesomeIcon" size="3x" icon={["fab", "linkedin"]} /></a>
             <a href="https://github.com/marvintaylor-dev/" target="_blank"><FontAwesomeIcon id="awesomeIcon" size="3x" icon={["fab", "github"]} /></a>
         </div>
     )
