d0a7f74a4892a6242567629811523c61855def3b
mt-dev <76923383+marvintaylor-dev@users.noreply.github.com>
2023-03-30 02:00:43 -0700

    Update links.components.jsx
    Major update to links

--- frontend/src/components/links/links.components.jsx
+++ frontend/src/components/links/links.components.jsx
@@ -1,14 +1,53 @@
 import React from 'react'
-
 import './links.styles.scss'
+import { NavLink } from 'react-router-dom'
 
-const Links = ({ open }) => (
-    <div className={`${open ? 'slideIn' : ''} header-links-container`}>
-        <a href="#projects" className="header-link">Projects</a>
-        <a href="#skills" className="header-link">Skills</a>
-        <a href="#education" className="header-link">Education</a>
-        <a href="#contact" className="header-link">Contact</a>
-    </div>
-)
+const projects = document.querySelector("#projects");
+
+const layoutEffect = () => {
+    window.scrollTo(0, 0)
+};
+
+const handleScroll = () => {
+    projects.scrollIntoView({ behavior: "smooth", block: "center" })
+}
+
+
+
+const Links = ({ open }) => {
+
+    return (
+        <div className={`${open ? 'slideIn' : ''} header-links-container`}>
+            <NavLink
+                to={"/"}
+                exact={true}
+                onClick={layoutEffect}
+                className="header-link"
+                activeClassName="selected" >
+                Home
+            </NavLink>
+            <a href="/#projects" className="header-link">Projects</a>
+            <a href="/#skills" className="header-link">Skills</a>
+            <NavLink
+                to={"/resume"}
+                exact={true}
+                onClick={layoutEffect}
+                className="header-link"
+                activeClassName="selected">
+                Resume
+            </NavLink>
+            {/* <a href="/contactpage" className="header-link">Contact</a> */}
+            <NavLink
+                to={"/aboutme"}
+                exact={true}
+                onClick={layoutEffect}
+                className="header-link"
+                activeClassName="selected">About Me
+            </NavLink>
+
+        </div>
+    )
+
+}
 
 export default Links;
